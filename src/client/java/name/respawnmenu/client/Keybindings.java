6e9c651415f508afc1387a2c50c73af7a52e75e2
George Skudder <george.skudder@hotmail.com>
2026-07-25 09:25:07 +0100

    Added localisation and custom keys (v1.1.1)

--- src/client/java/name/respawnmenu/client/Keybindings.java
+++ src/client/java/name/respawnmenu/client/Keybindings.java
@@ -1,11 +1,13 @@
 package name.respawnmenu.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
 public class Keybindings {
-    public static final KeyMapping OPEN_SCREEN = new KeyMapping(
-            "key.example.open_screen",
+    public static final KeyMapping OPEN_MENU = new KeyMapping(
+            "key.respawnmenu.openmenu",
+            InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_R,
             KeyMapping.Category.MISC
     );
--- src/client/java/name/respawnmenu/client/RespawnMenuClient.java
+++ src/client/java/name/respawnmenu/client/RespawnMenuClient.java
@@ -4,13 +4,16 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.network.chat.Component;
 
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+
 public class RespawnMenuClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (Keybindings.OPEN_SCREEN.consumeClick()) {
+			while (Keybindings.OPEN_MENU.consumeClick()) {
 				client.setScreenAndShow(new RespawnScreen(Component.empty()));
 			}
 		});
+		KeyMappingHelper.registerKeyMapping(Keybindings.OPEN_MENU);
 	}
 }
