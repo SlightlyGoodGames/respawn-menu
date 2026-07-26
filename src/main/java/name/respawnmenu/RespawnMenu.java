f1fd541fbbf16a4e34401d670e6adcc84e027680
George Skudder <george.skudder@hotmail.com>
2026-07-26 07:55:46 +0100

    Added /respawn command (v1.2.0+26.2)

--- src/main/java/name/respawnmenu/RespawnMenu.java
+++ src/main/java/name/respawnmenu/RespawnMenu.java
@@ -11,6 +11,9 @@ import net.minecraft.server.level.ServerPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import static net.minecraft.commands.Commands.literal;
+
 public class RespawnMenu implements ModInitializer {
 	public static final String MOD_ID = "respawn-menu";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -30,6 +33,27 @@ public class RespawnMenu implements ModInitializer {
 				);
             });
 		});
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(literal("respawn")
+					.executes(context -> {
+						ServerPlayer player = context.getSource().getPlayer();
+
+						if(player==null){
+							LOGGER.info("Could not respawn; player is null");
+							return 0;
+						}
+
+						LOGGER.info("Respawning " + player.getName().getString() + "...");
+						player.hurtServer(
+								player.level(),
+								player.level().damageSources().genericKill(),
+								Float.MAX_VALUE
+						);
+						return 1;
+					}));
+		});
+
 
 	}
 
